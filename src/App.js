959ca32e6b430e0878a42819fa811d00082bd35d
Ramzi Bouzaiene <ramzi.bouzaiene@sastec-group.com>
2023-02-19 16:04:50 +0100

    adding the app in build mode and offline

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import React, {useState, useEffect} from 'react';
+import React  from 'react';
 import './App.css';
+import video1 from "./videos/BalletInSmokeMusic_054599628.mp4"
+import video2 from "./videos/GraphicArtistMusic_052214820.mp4"
+import video3 from "./videos/SculptorWeldingMusic_053783433.mp4"
 
 function App() {
+  // This is for consuming videos from a public API for the videos
 
-  const [data, setData] =useState([]);
+  // const [data, setData] =useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("https://orangevalleycaa.org/api/videos")
-        .then(response => response.json());
-        setData(res);
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch("https://orangevalleycaa.org/api/videos")
+  //       .then(response => response.json());
+  //       setData(res);
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <div className="App">
       <header>
        <h1> Videos </h1>
       </header>
-      {data.map(video => (
-        <div key={video.id} className="video-container">
-          <h2>{video.name}</h2>
-          <video height={200} controls src={video.video_url} data-testid="video-test" />
+        <div className="video-container">
+          <video src={video1} controls height={200}/>
+          <video src={video2} controls height={200}/>
+          <video src={video3} controls height={200}/>
         </div>
-        ))}
     </div>
   );
 }
